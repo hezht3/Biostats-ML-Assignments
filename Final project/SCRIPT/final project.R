73e9fee99cd1c688dc1c53251b03ad3982b1c7c8
hezht3 <zhe33@jhu.edu>
2022-03-09 22:14:22 -0500

    revise linear model, commit tree based methods

--- Final project/SCRIPT/final project.R
+++ Final project/SCRIPT/final project.R
@@ -8,7 +8,10 @@ setwd("D:/OneDrive - Johns Hopkins/Course/140.644.01 - Statistical Machine Learn
 load("./DATA/nhanes2003-2004.Rda")
 
 
-# Data cleaning
+# Preparation
+
+
+## Data cleaning
 
 
 data <- nhanes2003_2004 %>% 
@@ -16,7 +19,7 @@ data <- nhanes2003_2004 %>%
     select(mortstat, RIDAGEYR, RIAGENDR, BPQ010, BPQ060, DIQ010, DIQ050, DIQ090, MCQ010, MCQ053,
            MCQ160A, MCQ160B, MCQ160K, MCQ160L, BMXWAIST, MCQ160M, MCQ220, MCQ245A, MCQ250A, MCQ250B,
            MCQ250C, MCQ250E, MCQ250F, MCQ250G, MCQ265, SSQ011, SSQ051, WHQ030, WHQ040, LBXRDW, HSD010,
-           BPXPULS, BPXML1, VIQ200, BMXBMI, BPXSY1, BPXDI1)
+           BPXPULS, BPXML1, VIQ200, BMXBMI, BPXSY1, BPXDI1)   # select predictors in instructions
 
 data <- data %>% 
     mutate(
@@ -111,17 +114,20 @@ data <- data %>%
     # laboratory
     mutate_at(vars(LBXRDW), funs(setattr(., "label", "Red cell distribution width (%)")))
 
-data <- data %>% drop_na()
+data <- data %>% 
+    mutate(BPXDI1 = ifelse(BPXDI1 <= 20, NA, BPXDI1))   # drop outliers in `BPXDI1`
+
+data <- data %>% drop_na()   # drop missing values
 
 
-# Exploratory analysis
+## Exploratory analysis
 
 
 data %>% 
     select(mortstat, RIDAGEYR, BMXWAIST, BPXML1, BPXSY1, BPXDI1, BMXBMI, LBXRDW) %>% 
     mutate(mortstat = factor(mortstat, labels = c("Alive", "Deceased"))) %>% 
     GGally::ggpairs(aes(color = mortstat, alpha = 0.5)) +
-    theme_bw()
+    theme_minimal()   # continuous variable
 
 data %>% 
     select(- RIDAGEYR, - BMXWAIST, - BPXML1, - BPXSY1, - BPXDI1, - BMXBMI, - LBXRDW) %>% 
@@ -130,10 +136,18 @@ data %>%
     geom_bar(position = "fill") +
     facet_wrap(vars(name), scales = "free", ncol = 2) +
     labs(x = NULL, y = NULL, fill = NULL) +
-    theme_bw()
+    theme_minimal()   # categorical variable
+
+data %>% 
+    select(-mortstat) %>% 
+    mutate(across(everything(),
+                  ~ as.numeric(.x))) %>% 
+    corrr::correlate() %>% 
+    corrr::rplot(colours = c("indianred2", "black", "skyblue1")) +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1))   # pair-wise correlation
 
 
-# Re-sampling
+## Re-sampling
 
 set.seed(123)
 data_split <- initial_split(data, strata = "mortstat", prop = 8/10)
@@ -141,18 +155,41 @@ data_split <- initial_split(data, strata = "mortstat", prop = 8/10)
 data_train <- training(data_split)
 data_test <- testing(data_split)
 
-data_fold <- vfold_cv(data_train, v = 10)
+data_fold <- vfold_cv(data_train, strata = "mortstat", v = 10)
 
 
-# Logistic regression
+# Linear model
 
-logistic_fit <- logistic_reg() %>%
+
+## Logistic regression
+
+
+### Fit logistic regression model
+
+
+logistic_recipe <- recipe(mortstat ~ ., data = data_train) %>% 
+    step_dummy(all_nominal(), - all_outcomes()) %>%
+    step_zv(all_numeric()) %>% 
+    step_corr(threshold = 0.8)   # specify recipe, exclude variables closely correlated (r = 0.8)
+
+logistic_spec <- logistic_reg() %>%
     set_engine("glm") %>% 
-    set_mode("classification") %>% 
-    fit(mortstat ~ ., data = data_train)
+    set_mode("classification")   # specify logistic model
+
+logistic_workflow <- 
+    workflow() %>% 
+    add_model(logistic_spec) %>% 
+    add_recipe(logistic_recipe)   # build logistic model workflow
+
+logistic_fit <- logistic_workflow %>%
+    fit(data = data_train)   # fit logistic model in training set
+
+
+### Evaluate model performance
+
 
 augment(logistic_fit, new_data = data_test) %>% 
-    conf_mat(truth = mortstat, estimate = .pred_class)
+    conf_mat(truth = mortstat, estimate = .pred_class)   # confusion matrix
 
 augment(logistic_fit, new_data = data_test) %>% 
     accuracy(truth = mortstat, estimate = .pred_class) %>% 
@@ -161,126 +198,348 @@ augment(logistic_fit, new_data = data_test) %>%
     bind_rows(augment(logistic_fit, new_data = data_test) %>% 
                   spec(truth = mortstat, estimate = .pred_class)) %>% 
     bind_rows(augment(logistic_fit, new_data = data_test) %>% 
-                  roc_auc(truth = mortstat, `.pred_Assumed alive`))
+                  roc_auc(truth = mortstat, `.pred_Assumed alive`))   # accuracy, sensitivity, specificity, auc
 
-### different threshold
 
-predictions <- logistic_fit %>%
-    predict(new_data = data_test, type = "prob")
+### Model post-processing
 
-data_test_pred <- bind_cols(predictions, data_test)
 
-threshold_data <- data_test_pred %>%
-    threshold_perf(mortstat, `.pred_Assumed alive`, thresholds = seq(0.5, 1, by = 0.0025))
-
-threshold_data <- threshold_data %>%
+data_threshold <- logistic_fit %>%
+    augment(new_data = data_test) %>% 
+    threshold_perf(mortstat, `.pred_Assumed alive`, thresholds = seq(0.5, 1, by = 0.0025)) %>%
     filter(.metric != "distance") %>%
-    mutate(group = case_when(
-        .metric == "sens" | .metric == "spec" ~ "1",
-        TRUE ~ "2"
-    ))
+    mutate(group = case_when(.metric == "sens" | .metric == "spec" ~ "1",
+                             TRUE ~ "2"))
 
-max_j_index_threshold <- threshold_data %>%
+max_j_index_threshold <- data_threshold %>%
     filter(.metric == "j_index") %>%
     filter(.estimate == max(.estimate)) %>%
     pull(.threshold)
 
-threshold_data %>% 
-    filter(.threshold %in% max_j_index_threshold) %>% 
-    arrange(.threshold)
-
-ggplot(threshold_data, aes(x = .threshold, y = .estimate, color = .metric, alpha = group)) +
+ggplot(data_threshold, aes(x = .threshold, y = .estimate, color = .metric, alpha = group)) +
     geom_line() +
-    theme_minimal() +
     scale_color_viridis_d(end = 0.9) +
     scale_alpha_manual(values = c(.4, 1), guide = "none") +
     geom_vline(xintercept = max_j_index_threshold, alpha = .6, color = "grey30") +
-    labs(
-        x = "'Good' Threshold\n(above this value is considered 'good')",
-        y = "Metric Estimate",
-        title = "Balancing performance by varying the threshold",
-        subtitle = "Vertical line = Max J-Index"
-    )
+    labs(x = "'Good' Threshold\n(above this value is considered 'good')",
+         y = "Metric Estimate",
+         title = "Balancing performance by varying the threshold",
+         subtitle = "Vertical line = Max J-Index") +
+    theme_minimal()
 
-# Lasso
+data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>% 
+    arrange(.threshold)
 
-logistic_model <- logistic_reg(penalty = tune(), mixture = 1) %>% 
-    set_engine("glmnet") %>% 
-    set_mode("classification")
+best_threshold <- data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>%
+    filter(.metric == "j_index") %>% 
+    pull(.threshold)
 
-logistic_receipt <- recipe(mortstat ~ ., data = data_train) %>% 
+data_preds_final <- logistic_fit %>%
+    augment(new_data = data_test) %>% 
+    mutate(.pred_class = make_two_class_pred(`.pred_Assumed alive`, levels(mortstat), 
+                                             threshold = best_threshold))
+
+data_preds_final %>% 
+    conf_mat(truth = mortstat, estimate = .pred_class)   # confusion matrix
+
+data_preds_final %>% 
+    accuracy(truth = mortstat, estimate = .pred_class) %>% 
+    bind_rows(data_preds_final %>% sens(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% spec(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% roc_auc(truth = mortstat, `.pred_Assumed alive`))   # accuracy, sensitivity, specificity, auc
+
+
+## Lasso
+
+
+### Fit lasso regression model
+
+
+lasso_recipe <- recipe(mortstat ~ ., data = data_train) %>% 
     step_dummy(all_nominal(), - all_outcomes()) %>%
     step_zv(all_numeric()) %>% 
-    step_normalize(all_predictors())
+    step_normalize(all_predictors())   # specify recipe
 
-logistic_workflow <- 
+lasso_spec <- logistic_reg(penalty = tune(), mixture = 1) %>% 
+    set_engine("glmnet") %>% 
+    set_mode("classification")   # specify lasso model
+
+lasso_workflow <- 
     workflow() %>% 
-    add_model(logistic_model) %>% 
-    add_recipe(logistic_receipt)
+    add_model(lasso_spec) %>% 
+    add_recipe(lasso_recipe)   # build lasso model workflow
 
-penalty_grid <- grid_regular(penalty(range = c(-4, 2)), levels = 50)
+penalty_grid <- grid_regular(penalty(range = c(-4, 2)), levels = 50)   # tuning parameter
 
-tune_res <- logistic_workflow %>% 
+tune_res <- lasso_workflow %>% 
     tune_grid(
     resamples = data_fold, 
     grid = penalty_grid,
     control = control_grid(save_pred = TRUE)
 )
 
-autoplot(tune_res)
+autoplot(tune_res) + theme_minimal()
 
-best_penalty <- select_best(tune_res, metric = "accuracy")
+best_penalty <- select_best(tune_res, metric = "roc_auc")
 
-logistic_final <- finalize_workflow(logistic_workflow, best_penalty)
+lasso_final <- finalize_workflow(lasso_workflow, best_penalty)
 
-logistic_final_fit <- fit(logistic_final, data = data_train)
+lasso_final_fit <- fit(lasso_final, data = data_train)
 
-augment(logistic_final_fit, new_data = data_test) %>% 
+
+### Evaluate model performance
+
+
+data_laaso_preds <- augment(lasso_final_fit, new_data = data_test)
+
+data_laaso_preds %>% 
     conf_mat(truth = mortstat, estimate = .pred_class)
 
-augment(logistic_final_fit, new_data = data_test) %>% 
+data_laaso_preds %>% 
     accuracy(truth = mortstat, estimate = .pred_class) %>% 
-    bind_rows(augment(logistic_final_fit, new_data = data_test) %>% 
-                  sens(truth = mortstat, estimate = .pred_class)) %>% 
-    bind_rows(augment(logistic_final_fit, new_data = data_test) %>% 
-                  spec(truth = mortstat, estimate = .pred_class)) %>% 
-    bind_rows(augment(logistic_final_fit, new_data = data_test) %>% 
-                  roc_auc(truth = mortstat, `.pred_Assumed alive`))
+    bind_rows(data_laaso_preds %>% sens(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_laaso_preds %>% spec(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_laaso_preds %>% roc_auc(truth = mortstat, `.pred_Assumed alive`))
 
-### different threshold https://probably.tidymodels.org/articles/where-to-use.html
-predictions <- logistic_final_fit %>%
-    predict(new_data = data_test, type = "prob")
 
-data_test_pred <- bind_cols(predictions, data_test)
+### Model post-processing
 
-threshold_data <- data_test_pred %>%
-    threshold_perf(mortstat, `.pred_Assumed alive`, thresholds = seq(0.5, 1, by = 0.0025))
 
-threshold_data <- threshold_data %>%
+data_threshold <- lasso_final_fit %>%
+    augment(new_data = data_test) %>% 
+    threshold_perf(mortstat, `.pred_Assumed alive`, thresholds = seq(0.5, 1, by = 0.0025)) %>%
     filter(.metric != "distance") %>%
-    mutate(group = case_when(
-        .metric == "sens" | .metric == "spec" ~ "1",
-        TRUE ~ "2"
-    ))
+    mutate(group = case_when(.metric == "sens" | .metric == "spec" ~ "1",
+                             TRUE ~ "2"))
 
-max_j_index_threshold <- threshold_data %>%
+max_j_index_threshold <- data_threshold %>%
     filter(.metric == "j_index") %>%
     filter(.estimate == max(.estimate)) %>%
     pull(.threshold)
 
-threshold_data %>% 
-    filter(.threshold %in% max_j_index_threshold) %>% 
-    arrange(.threshold)
-
-ggplot(threshold_data, aes(x = .threshold, y = .estimate, color = .metric, alpha = group)) +
+ggplot(data_threshold, aes(x = .threshold, y = .estimate, color = .metric, alpha = group)) +
     geom_line() +
-    theme_minimal() +
     scale_color_viridis_d(end = 0.9) +
     scale_alpha_manual(values = c(.4, 1), guide = "none") +
     geom_vline(xintercept = max_j_index_threshold, alpha = .6, color = "grey30") +
-    labs(
-        x = "'Good' Threshold\n(above this value is considered 'good')",
-        y = "Metric Estimate",
-        title = "Balancing performance by varying the threshold",
-        subtitle = "Vertical line = Max J-Index"
-    )
+    labs(x = "'Good' Threshold\n(above this value is considered 'good')",
+         y = "Metric Estimate",
+         title = "Balancing performance by varying the threshold",
+         subtitle = "Vertical line = Max J-Index") +
+    theme_minimal()
+
+data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>% 
+    arrange(.threshold)
+
+best_threshold <- data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>%
+    filter(.metric == "j_index") %>% 
+    pull(.threshold)
+
+data_preds_final <- lasso_final_fit %>%
+    augment(new_data = data_test) %>% 
+    mutate(.pred_class = make_two_class_pred(`.pred_Assumed alive`, levels(mortstat), 
+                                             threshold = best_threshold))
+
+data_preds_final %>% 
+    conf_mat(truth = mortstat, estimate = .pred_class)   # confusion matrix
+
+data_preds_final %>% 
+    accuracy(truth = mortstat, estimate = .pred_class) %>% 
+    bind_rows(data_preds_final %>% sens(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% spec(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% roc_auc(truth = mortstat, `.pred_Assumed alive`))   # accuracy, sensitivity, specificity, auc
+
+
+# Tree-Based Methods
+
+
+## RandomForest
+
+
+rf_recipe <- recipe(mortstat ~ ., data = data_train) %>% 
+    step_dummy(all_nominal(), - all_outcomes()) %>%
+    step_zv(all_numeric())   # specify recipe
+
+rf_spec <- rand_forest(mtry = 6) %>%
+    set_engine("randomForest", importance = TRUE) %>%
+    set_mode("classification")   # specify random forests model
+
+rf_workflow <- 
+    workflow() %>% 
+    add_model(rf_spec) %>% 
+    add_recipe(rf_recipe)   # build random forests model workflow
+
+rf_fit <- rf_workflow %>%
+    fit(data = data_train)
+
+data_preds <- rf_fit %>%
+    augment(new_data = data_test)
+
+data_preds %>% 
+    conf_mat(truth = mortstat, estimate = .pred_class)   # confusion matrix
+
+data_preds %>% 
+    accuracy(truth = mortstat, estimate = .pred_class) %>% 
+    bind_rows(data_preds %>% sens(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds %>% spec(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds %>% roc_auc(truth = mortstat, `.pred_Assumed alive`))   # accuracy, sensitivity, specificity, auc
+
+data_threshold <- rf_fit %>%
+    augment(new_data = data_test) %>% 
+    threshold_perf(mortstat, `.pred_Assumed alive`, thresholds = seq(0.5, 1, by = 0.0025)) %>%
+    filter(.metric != "distance") %>%
+    mutate(group = case_when(.metric == "sens" | .metric == "spec" ~ "1",
+                             TRUE ~ "2"))
+
+max_j_index_threshold <- data_threshold %>%
+    filter(.metric == "j_index") %>%
+    filter(.estimate == max(.estimate)) %>%
+    pull(.threshold)
+
+ggplot(data_threshold, aes(x = .threshold, y = .estimate, color = .metric, alpha = group)) +
+    geom_line() +
+    scale_color_viridis_d(end = 0.9) +
+    scale_alpha_manual(values = c(.4, 1), guide = "none") +
+    geom_vline(xintercept = max_j_index_threshold, alpha = .6, color = "grey30") +
+    labs(x = "'Good' Threshold\n(above this value is considered 'good')",
+         y = "Metric Estimate",
+         title = "Balancing performance by varying the threshold",
+         subtitle = "Vertical line = Max J-Index") +
+    theme_minimal()
+
+data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>% 
+    arrange(.threshold)
+
+best_threshold <- data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>%
+    filter(.metric == "j_index") %>% 
+    pull(.threshold)
+
+data_preds_final <- rf_fit %>%
+    augment(new_data = data_test) %>% 
+    mutate(.pred_class = make_two_class_pred(`.pred_Assumed alive`, levels(mortstat), 
+                                             threshold = best_threshold))
+
+data_preds_final %>% 
+    conf_mat(truth = mortstat, estimate = .pred_class)   # confusion matrix
+
+data_preds_final %>% 
+    accuracy(truth = mortstat, estimate = .pred_class) %>% 
+    bind_rows(data_preds_final %>% sens(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% spec(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% roc_auc(truth = mortstat, `.pred_Assumed alive`))   # accuracy, sensitivity, specificity, auc
+
+## Boosting trees
+
+### XGBoost model specification
+
+xgb_spec <- boost_tree(
+    trees = 1000, 
+    tree_depth = tune(), min_n = tune(), 
+    loss_reduction = tune(),                     ## first three: model complexity
+    sample_size = tune(), mtry = tune(),         ## randomness
+    learn_rate = tune()                        ## step size
+) %>% 
+    set_engine("xgboost") %>% 
+    set_mode("classification")
+
+xgb_grid <- grid_latin_hypercube(
+    tree_depth(),
+    min_n(),
+    loss_reduction(),
+    sample_size = sample_prop(),
+    finalize(mtry(), data_train),
+    learn_rate(),
+    size = 30
+)
+
+xgb_wf <- workflow() %>%
+    add_formula(mortstat ~ .) %>%
+    add_model(xgb_spec)
+
+doParallel::registerDoParallel()
+
+set.seed(234)
+xgb_res <- tune_grid(
+    xgb_wf,
+    resamples = data_fold,
+    grid = xgb_grid,
+    control = control_grid(save_pred = TRUE)
+)
+
+xgb_res
+
+best_auc <- select_best(xgb_res, "roc_auc")
+best_auc
+
+final_xgb <- finalize_workflow(
+    xgb_wf,
+    best_auc
+)
+
+final_xgb
+
+final_xgb_fit <- fit(final_xgb, data = data_train)
+
+data_preds <- final_xgb_fit %>%
+    augment(new_data = data_test)
+
+data_preds %>% 
+    conf_mat(truth = mortstat, estimate = .pred_class)   # confusion matrix
+
+data_preds %>% 
+    accuracy(truth = mortstat, estimate = .pred_class) %>% 
+    bind_rows(data_preds %>% sens(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds %>% spec(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds %>% roc_auc(truth = mortstat, `.pred_Assumed alive`))   # accuracy, sensitivity, specificity, auc
+
+data_threshold <- final_xgb_fit %>%
+    augment(new_data = data_test) %>% 
+    threshold_perf(mortstat, `.pred_Assumed alive`, thresholds = seq(0.5, 1, by = 0.0025)) %>%
+    filter(.metric != "distance") %>%
+    mutate(group = case_when(.metric == "sens" | .metric == "spec" ~ "1",
+                             TRUE ~ "2"))
+
+max_j_index_threshold <- data_threshold %>%
+    filter(.metric == "j_index") %>%
+    filter(.estimate == max(.estimate)) %>%
+    pull(.threshold)
+
+ggplot(data_threshold, aes(x = .threshold, y = .estimate, color = .metric, alpha = group)) +
+    geom_line() +
+    scale_color_viridis_d(end = 0.9) +
+    scale_alpha_manual(values = c(.4, 1), guide = "none") +
+    geom_vline(xintercept = max_j_index_threshold, alpha = .6, color = "grey30") +
+    labs(x = "'Good' Threshold\n(above this value is considered 'good')",
+         y = "Metric Estimate",
+         title = "Balancing performance by varying the threshold",
+         subtitle = "Vertical line = Max J-Index") +
+    theme_minimal()
+
+data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>% 
+    arrange(.threshold)
+
+best_threshold <- data_threshold %>% 
+    filter(.threshold %in% max_j_index_threshold) %>%
+    filter(.metric == "j_index") %>% 
+    pull(.threshold)
+
+data_preds_final <- final_xgb_fit %>%
+    augment(new_data = data_test) %>% 
+    mutate(.pred_class = make_two_class_pred(`.pred_Assumed alive`, levels(mortstat), 
+                                             threshold = best_threshold))
+
+data_preds_final %>% 
+    conf_mat(truth = mortstat, estimate = .pred_class)   # confusion matrix
+
+data_preds_final %>% 
+    accuracy(truth = mortstat, estimate = .pred_class) %>% 
+    bind_rows(data_preds_final %>% sens(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% spec(truth = mortstat, estimate = .pred_class)) %>% 
+    bind_rows(data_preds_final %>% roc_auc(truth = mortstat, `.pred_Assumed alive`))   # accuracy, sensitivity, specificity, auc
